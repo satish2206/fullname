897a1bf4e1f5cbf3afd4493ffda790e597820ea7
Satish Gade <satishgade2206@gmail.com>
2023-12-30 14:19:11 +0400

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import * as ReactTooltip from "react-tooltip";
-
 
 
 export default function App() {
   const [firstname, setFirstName] = useState("");
   const [lastname, setLastName] = useState("");
   const [fullName, setFullName] = useState("");
-  const [showFirstNameTooltip, setShowFirstNameTooltip] = useState(false);
-  const [showLastNameTooltip, setShowLastNameTooltip] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,26 +13,21 @@ export default function App() {
       const fullName = `${firstname} ${lastname}`;
       setFullName(fullName);
     } else {
-      setFullName("");
-
-      // Show tooltips for empty fields
-      setShowFirstNameTooltip(firstname.trim() === "");
-      setShowLastNameTooltip(lastname.trim() === "");
+      setFullName(""); // Clear the full name if fields are empty
     }
   };
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
-    setShowFirstNameTooltip(false); // Clear tooltip when user types
   };
 
   const handleLastNameChange = (e) => {
     setLastName(e.target.value);
-    setShowLastNameTooltip(false); // Clear tooltip when user types
   };
 
   return (
     <div>
+      <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <label>
           First Name:
@@ -44,18 +35,9 @@ export default function App() {
             type="text"
             value={firstname}
             onChange={handleFirstNameChange}
-            data-tip={showFirstNameTooltip && "Please fill out this field"}
-            data-for="firstname-tooltip"
+            required
           />
         </label>
-        <ReactTooltip
-          id="firstname-tooltip"
-          place="top"
-          type="error"
-          effect="solid"
-        >
-          {showFirstNameTooltip && "Please fill out this field"}
-        </ReactTooltip>
         <br />
         <label>
           Last Name:
@@ -63,18 +45,9 @@ export default function App() {
             type="text"
             value={lastname}
             onChange={handleLastNameChange}
-            data-tip={showLastNameTooltip && "Please fill out this field"}
-            data-for="lastname-tooltip"
+            required
           />
         </label>
-        <ReactTooltip
-          id="lastname-tooltip"
-          place="top"
-          type="error"
-          effect="solid"
-        >
-          {showLastNameTooltip && "Please fill out this field"}
-        </ReactTooltip>
         <br />
         <button type="submit">Submit</button>
       </form>
