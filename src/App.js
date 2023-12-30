e5c567f217993486432135024b8c10c8a5e55c0d
Satish Gade <satishgade2206@gmail.com>
2023-12-30 13:21:33 +0400

    still

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import React, { useState } from "react";
 
 
-const Tooltip = ({ show, message }) => {
-  return show ? <div className="tooltip">{message}</div> : null;
-};
-
 export default function App() {
   const [firstname, setFirstName] = useState("");
   const [lastname, setLastName] = useState("");
   const [fullName, setFullName] = useState("");
-  const [firstNameError, setFirstNameError] = useState(false);
-  const [lastNameError, setLastNameError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (firstname.trim() === "") {
-      setFirstNameError(true);
-    } else {
-      setFirstNameError(false);
-    }
-
-    if (lastname.trim() === "") {
-      setLastNameError(true);
-    } else {
-      setLastNameError(false);
-    }
-
     if (firstname.trim() !== "" && lastname.trim() !== "") {
       const fullName = `${firstname} ${lastname}`;
-      setFullName(fullName); // Update state with full name
-      console.log("Submitted:", fullName);
+      setFullName(fullName);
+    } else {
+      setFullName(""); // Clear the full name if fields are empty
     }
   };
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
-    setFullName(`${e.target.value} ${lastname}`);
-    setFirstNameError(false); // Clear error when user types in the field
   };
 
   const handleLastNameChange = (e) => {
     setLastName(e.target.value);
-    setFullName(`${firstname} ${e.target.value}`);
-    setLastNameError(false); // Clear error when user types in the field
   };
 
   return (
     <div>
-      <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
         <label>
           First Name:
@@ -57,22 +35,16 @@ export default function App() {
             value={firstname}
             onChange={handleFirstNameChange}
           />
-          <Tooltip show={firstNameError} message="Please fill out the First Name field" />
         </label>
         <br />
         <label>
           Last Name:
-          <input
-            type="text"
-            value={lastname}
-            onChange={handleLastNameChange}
-          />
-          <Tooltip show={lastNameError} message="Please fill out the Last Name field" />
+          <input type="text" value={lastname} onChange={handleLastNameChange} />
         </label>
         <br />
-        <p>Full Name: {fullName}</p>
         <button type="submit">Submit</button>
       </form>
+      <p>Full Name: {fullName}</p>
     </div>
   );
 }
